2b89ac88beaf15a043f77e065439f7f81b0a5fd7
Mohammedpashaf <145536112+Mohammedpashaf@users.noreply.github.com>
2024-11-09 14:22:29 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,png}"],
+  theme: {
+    extend: {
+      gridRow: {
+        'span-16': 'span 16 / span 16',
+      },
+      gridRowStart: {
+        '8': '8',
+        '9': '9',
+        '10': '10',
+        '11': '11',
+        '12': '12',
+        '13': '13',
+      },
+      gridRowEnd: {
+        '8': '8',
+        '9': '9',
+        '10': '10',
+        '11': '11',
+        '12': '12',
+        '13': '13',
+      }
+    },
+  },
+  plugins: [],
+}
