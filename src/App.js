b19880b19cf8b64f27cf10ef338f5805ef632404
Mohammedpashaf <145536112+Mohammedpashaf@users.noreply.github.com>
2024-11-15 18:47:03 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import './App.css';
-import Footer from './components/common/Footer';
-import NavBar from './components/common/NavBar';
 import MainSection from './components/MainSection';
+import SecondPage from './components/SecondPage';
 import './custom.css';
 import './output.css';
+
+
 function App() {
   return (
     <div className='bg-name'>
-      <NavBar></NavBar>
-      <MainSection></MainSection>
-      <Footer></Footer>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainSection />}></Route>
+          <Route path="SecondPage" element={<SecondPage />} />
+          <Route path="MainSection" element={<MainSection />} />
+          <Route path="*" element={<MainSection />} />
+          
+        </Routes>
+      </BrowserRouter>
+      <div >
+        <Outlet/>
+        {/* <MainSection></MainSection> */}
+        {/* <SecondPage></SecondPage> */}
+      </div>
     </div>
   );
 }
