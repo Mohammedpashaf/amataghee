7e3f910704ce227d6bca0d93fa9dbfb948a04afe
Mohammedpashaf <145536112+Mohammedpashaf@users.noreply.github.com>
2024-11-15 19:22:12 +0530

    Add files via upload

---
+++ src/components/common/FooterSecond.jsx
@@ -0,0 +1,50 @@
+
+
+function Footer() {
+  return (
+    <div>
+      <footer class="py-14 px-16 font-sans tracking-wide relative">
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+
+          <div>
+            <h2 class="text-lg text-black-300 font-semibold mb-6">Our Links</h2>
+            <ul class="space-y-5">
+              <li><a href="" class="text-black-300 hover:text-white text-base flex items-center transition-all">Lorem lpsum</a></li>
+              <li><a href="" class="text-black-300 hover:text-white text-base flex items-center transition-all">Lorem Our lpsum</a></li>
+              <li><a href="" class="text-black-300 hover:text-white text-base flex items-center transition-all">Our Lorem</a></li>
+              <li><a href="" class="text-black-300 hover:text-white text-base flex items-center transition-all">Loremdjs</a></li>
+              <li><a href="" class="text-black-300 hover:text-white text-base flex items-center transition-all">Lorem lpsum</a></li>
+            </ul>
+            </div>
+            <div>
+            <h2 class="text-lg text-black-300 font-semibold mb-6">Our Links</h2>
+            <ul class="space-y-5">
+              <li><a href="" class="text-black-300 hover:text-white text-base flex items-center transition-all">Lorem lpsum</a></li>
+              <li><a href="" class="text-black-300 hover:text-white text-base flex items-center transition-all">Lorem Our lpsum</a></li>
+              <li><a href="" class="text-black-300 hover:text-white text-base flex items-center transition-all">Our Lorem</a></li>
+              <li><a href="" class="text-black-300 hover:text-white text-base flex items-center transition-all">Loremdjs</a></li>
+              <li><a href="" class="text-black-300 hover:text-white text-base flex items-center transition-all">Lorem lpsum</a></li>
+            </ul>
+            </div>
+            <div>
+            <h2 class="text-lg text-black-300 font-semibold mb-6">Our Links</h2>
+            <ul class="space-y-5">
+              <li><a href="" class="text-black-300 hover:text-white text-base flex items-center transition-all">Lorem lpsum</a></li>
+              <li><a href="" class="text-black-300 hover:text-white text-base flex items-center transition-all">Lorem Our lpsum</a></li>
+              <li><a href="" class="text-black-300 hover:text-white text-base flex items-center transition-all">Our Lorem</a></li>
+              <li><a href="" class="text-black-300 hover:text-white text-base flex items-center transition-all">Loremdjs</a></li>
+              <li><a href="" class="text-black-300 hover:text-white text-base flex items-center transition-all">Lorem lpsum</a></li>
+            </ul>
+            </div>
+          <div class="items-center lg:justify-center">
+            <h1 className="">Amata</h1>
+            <p class="text-black-300 text-sm">Placerat vestibulum lectus mauris ultrices. Felis bibendum ut tristique 
+            et egestas quis ipsum.</p>
+             
+          </div>
+        </div>        
+      </footer>
+    </div>
+  )
+}
+export default Footer;
